ec8f4f1e7d893554a3c80e7a2c6558ebc0127af9
Alex Gustafson <alex@remarkvisions.com>
2016-04-23 16:53:49 +0000

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pins
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "/users/omniauth_callbacks" }
   root 'home#index'
   get 'home/about'
 
