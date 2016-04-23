11ebef63069a9059b5a80d7dc18b17f5ce1f955c
Alex Gustafson <alex@remarkvisions.com>
2016-04-23 16:37:31 +0000

    user.rb changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@ end
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
       # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"]) 
+      @user = User.find_for_google_oauth2(request.env[“omniauth.auth”], current_user)
 
       if @user.persisted?
+        session[:sn_user] = request.env[‘omniauth.params’]
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in_and_redirect @user, :event => :authentication
       else
@@ -33,4 +34,46 @@ def self.from_omniauth(access_token)
     #     )
     # end
     user
+end
+
+def self.find_for_google_oauth2(access_token, signed_in_resource = nil)
+
+data = access_token.info
+
+user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+
+if user
+
+return user
+
+else
+
+registered_user = User.where(:email => access_token.info.email).first
+
+if registered_user
+
+return registered_user
+
+else
+
+access_token.provider = “Google”
+
+user = User.create(first_name: data[“first_name”],
+
+last_name: data[“last_name”],
+
+provider:access_token.provider,
+
+email: data[“email”],
+
+password: Devise.friendly_token[0,20],
+
+confirmed_at:Time.zone.now # if u don’t want to send any confirmation mail
+
+)
+
+end
+
+end
+
 end
